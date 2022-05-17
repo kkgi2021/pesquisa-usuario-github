233591db1704be980b90f8a5a40b9f53603e8143
claiton gimenis <cacagimenis@gmail.com>
2022-05-16 23:29:49 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./styles.css";
- 
+
 //--------------------------------------------------
 export default function App(props) {
-  const[ usuario, setUsuario ] = useState('');
-   function handlePesquisa(){
-     axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response));
-   }
-    return (  
-     
-     
-//<----------- não apague as linaha acima --------------------------->     
-   < >
-      
-      <input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
-      <button type="button" onClick={handlePesquisa} >pesquisar</button>
-     
-   </>
-
-
-
-/*<------------- não apague as linhas abaixo --------------------> */
-  ); 
+  const [usuario, setUsuario] = useState("");
+  function handlePesquisa() {
+    axios
+      .get(`https://api.github.com/users/${usuario}/repos`)
+      .then((response) => console.log(response));
+  }
+  return (
+    <>
+      <input
+        className="usuarioInput"
+        placeholder="Usuário"
+        value={usuario}
+        onChange={(e) => setUsuario(e.target.value)}
+      />
+      <button type="button" onClick={handlePesquisa}>
+        pesquisar
+      </button>
+    </>
+  );
 }
